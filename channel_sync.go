214226693b80d4d37fc7c4c4f1a471b00a189f41
sayalipol <sayalip1506@gmail.com>
2025-04-18 18:27:40 +0530

    comments added

--- channel_sync.go
+++ channel_sync.go
@@ -3,7 +3,8 @@ import (
 	"fmt"
 	"time"
 )
-
+//This is the function we’ll run in a goroutine. 
+// The done channel will be used to notify another goroutine that this function’s work is done.
 func f(c chan bool){
 	fmt.Println("working...")
 	time.Sleep(time.Second)
@@ -13,5 +14,5 @@ func f(c chan bool){
 func main(){
 	c :=make(chan bool, 1)
 	go f(c)
-	<-c
+	<-c		//Block until we receive a notification from the worker on the channel.
 }
