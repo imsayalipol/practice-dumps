0bf054abdbe63c556bc9c73392fdb9896563fa7e
sayalipol <sayalip1506@gmail.com>
2025-04-19 18:05:37 +0530

    closing channel

---
+++ closing_channel.go
@@ -0,0 +1,32 @@
+package main
+import "fmt"
+
+func main(){
+	jobs :=make(chan int,5)
+	done :=make(chan bool)
+
+	go func (){
+		for{
+			j, ok := <-jobs
+			if ok{
+				fmt.Println("Job received :", j)
+			}else{
+				fmt.Println("No more jobs")
+				done<-true
+				return
+			}			
+		}
+	}()
+
+	for j:=1; j<4; j++{
+		jobs<-j
+		fmt.Println("Job sent :", j)
+	}
+	close(jobs)
+	fmt.Println("All jobs sent !!!")
+
+	<-done
+
+	_, ok := <-jobs
+	fmt.Println("More jobs available? : ", ok)
+}
