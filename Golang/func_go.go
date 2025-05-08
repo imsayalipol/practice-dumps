2175db4744b0b84e672056091b68af0053e0b2c4
sayalipol <sayalip1506@gmail.com>
2025-05-08 11:28:20 +0530

    Refactor main function to include named function assignment for square calculation

--- Golang/func_go.go
+++ Golang/func_go.go
@@ -11,4 +11,12 @@ func main() {
 		return x * x
 	}
 	fmt.Println(f(5)) // Output: 25
+
+	// you can also assign name
+	f = square
+	fmt.Println(f(10))
+}
+
+func square(i int) int {
+	return i * i
 }
